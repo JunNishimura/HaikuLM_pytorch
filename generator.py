6ac53e365b975bc37f099b4e6069287cd68ac1eb
Jun Nishimura <n.junjun0303@gmail.com>
2021-08-12 21:36:24 +0900

    ADD haiku generator

---
+++ generator.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn.functional as F
+import pandas as pd
+import numpy as np
+
+from params import *
+from preprocessor import Preprocessor
+from model import HaikuModel
+
+def generate():
+    # 俳句DataFrameの取得
+    try:
+        df = pd.read_pickle(HAIKU_PKL_PATH)
+    except:
+        import pickle
+        with open(HAIKU_PKL_PATH, 'rb') as f:
+            df = pickle.load(f)
+    
+    # 俳句リストの取得
+    haiku_list = df.haiku.tolist()
+
+    # preprocessor 
+    prepro = Preprocessor(SEQ_LENGTH)
+    prepro.fit(haiku_list)
+
+    # modelの構築
+    VOCAB_SIZE = len(prepro.char_to_id)
+    model = HaikuModel(VOCAB_SIZE, EMBEDDING_DIM, HIDDEN_SIZE, NUM_LAYERS)
+    
+    # modelのload
+    model.load_state_dict(torch.load('./checkpoints/ckpt_10.pt')['model_state_dict'])
+    model.eval()
+
+    # inference
+    generated_haikulist = []
+    next_char = "\n"
+    with torch.no_grad():
+        # 指定した回数だけ俳句を生成する
+        while len(generated_haikulist) < HAIKU_NUM:
+            states = model.initHidden(batch_size=1) # inference時のbatch sizeは1
+            haiku = ''
+
+            while True:
+                input_id = [[prepro.char_to_id[next_char]]]
+                input_tensor = torch.tensor(input_id, device=model.device)
+                logits, states = model(input_tensor, states)
+                probs = F.softmax(torch.squeeze(logits)).cpu().detach().numpy()
+                next_id = np.random.choice(VOCAB_SIZE, p=probs)
+                next_char = prepro.id_to_char[next_id]
+
+                # 改行が出たら俳句の完成合図
+                if next_char == '\n':
+                    break
+                else:
+                    haiku += next_char
+            
+            generated_haikulist.append(haiku)
+            print(haiku)
+
+if __name__=="__main__":
+    generate()
